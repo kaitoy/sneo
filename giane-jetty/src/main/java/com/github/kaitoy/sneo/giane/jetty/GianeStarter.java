73d4079795c7cdabf6ff0581459979e0faab3cb2
kaitoy <kaitoy@pcap4j.org>
2013-12-05 12:57:15 -0700

    Add an option to specify HTTP port.

--- giane-jetty/src/main/java/com/github/kaitoy/sneo/giane/jetty/GianeStarter.java
+++ giane-jetty/src/main/java/com/github/kaitoy/sneo/giane/jetty/GianeStarter.java
@@ -15,12 +15,28 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class GianeStarter {
 
   public static void main(String[] args) throws Exception {
-    Server server = new Server(8080);
+    int httpPort = 8080;
+
+    for (int i = 0; i < args.length; i++) {
+      String arg = args[i];
+      try {
+        if ("--httpPort".equals(arg)) {
+          httpPort = Integer.parseInt(args[++i]);
+        } else {
+          System.err.println("Invalid option: " + arg);
+          System.exit(1);
+        }
+      } catch (Exception e) {
+        System.err.println("An error occurred in processing an option: " + arg);
+        System.exit(1);
+      }
+    }
+
+    Server server = new Server(httpPort);
 
     WebAppContext context = new WebAppContext();
     context.setServer(server);
     context.setContextPath("/giane");
-    //context.setWelcomeFiles(new String[] {"index.action"});
 
     ProtectionDomain protectionDomain = GianeStarter.class.getProtectionDomain();
     URL location = protectionDomain.getCodeSource().getLocation();
